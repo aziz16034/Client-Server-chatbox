46bae322875abec56a7e80f7dc45d150640d3a27
aziz16034 <67529117+aziz16034@users.noreply.github.com>
2020-07-06 01:12:19 -0500

    Django

---
+++ django_project/blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'Blog/Home.html')
+
+
+
+def home(request):
+    return render(request, 'Blog/about.html')
+
+
