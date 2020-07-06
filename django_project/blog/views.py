c689cfdad76cd5bcd81b34ffe688dd437a40a104
aziz16034 <67529117+aziz16034@users.noreply.github.com>
2020-07-06 04:12:44 -0500

    Updated Django

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,13 +2,33 @@ from django.shortcuts import render
 
 # Create your views here.
 
+posts =[
+    {
 
+    'author':'Abdul Aziz',
+    'title': 'Blog Post 1',
+    'content':'First post content',
+    'date_posted': 'August 28 2020'
+},
+    {
+   'author':'Abdul Rehman',
+    'title': 'Blog Post 2',
+    'content':'Second post content',
+    'date_posted': 'August 29 2020'
+}
+]
 def home(request):
-    return render(request, 'Blog/Home.html')
+    context = {
+        'posts':posts,
+    }
+    return render(request, 'blog/home.html', context)
 
 
 
-def home(request):
-    return render(request, 'Blog/about.html')
+def about(request):
+    return render(request, 'blog/about.html', {'title':'About'})
+
+def contact_us(request):
+    return render(request, 'blog/contact.html')
 
 
